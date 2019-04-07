2cf577e09e5e127dd2c76209debf476ff00e20c6
Sean Pianka <pianka@eml.cc>
2019-04-07 19:04:29 -0400

    Update database rebuild script

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ import sqlservice
 import sqlalchemy
 
 
-with open('env_vars.json') as f:
+with open("env_vars.json") as f:
     env_vars = json.load(f)["Variables"]
 
 for k, v in env_vars.items():
@@ -13,13 +13,37 @@ for k, v in env_vars.items():
 
 import sa
 
-c = sqlservice.SQLClient(
+db = sqlservice.SQLClient(
     {"SQL_DATABASE_URI": sa.constants.SQLALCHEMY_DATABASE_URI},
     model_class=sa.models.Model,
 )
 
-c.drop_all()
-c.create_all()
-c.commit()
+db.drop_all()
+db.create_all()
+db.commit()
 
-#except sqlalchemy.orm.exc.NoResultFound:
+c = sa.models.College(name="Florida State University", email_tld="my.fsu.edu")
+db.College.save(c)
+
+with open("advisors.json") as f:
+    advisors_json = json.load(f)
+
+for major, advisors in advisors_json.items():
+    m = sa.models.Major(name=major, college_id=c.id)
+    db.Major.save(m)
+    db.commit()
+
+    for advisor in advisors:
+        for student_level in advisor["advises_for"]:
+            a = sa.models.Advisor(
+                name=advisor["name"],
+                email=advisor["email"],
+                is_undergraduate=student_level.lower() == "Undergraduate",
+                major_id=m.id
+            )
+            db.Advisor.save(a)
+
+    db.Major.save(m)
+
+db.commit()
+# except sqlalchemy.orm.exdb.NoResultFound:
